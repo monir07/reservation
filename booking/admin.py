4dead8d8762eba4cab0ceb075ec52a2911376bbb
vaibhavmule <vaibhavmule135@gmail.com>
2016-04-10 21:10:11 +0530

    add train model

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from booking.models import Train
+
+admin.site.register(Train)
--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Train(models.Model):
+    number = models.IntegerField()
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
